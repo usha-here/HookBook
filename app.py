41568c5fb39d772b204df8edbfe5072487a845f9
usha-here <usha.20225099@mnnit.ac.in>
2025-08-30 02:14:21 +0530

    Add Flask app and HTML template for book recommender system

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import pickle
 
-app=Flask(__name__)
+popular_df = pickle.load(open('popular.pkl','rb'))
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask!"
+    return render_template('index.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_ratings'].values),
+                           available = list(popular_df['Availability'].values)
+                           )
 if __name__ == '__main__':
     app.run(debug=True)
